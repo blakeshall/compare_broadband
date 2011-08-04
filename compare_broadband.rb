aec31aded043f28fb3ddd3ee32ba2ab1b241eb35
blakeshall <blake@blakeshall.com>
2011-08-04 14:15:04 -0700

    Added geocodind and broadband_map lookup

--- compare_broadband.rb
+++ compare_broadband.rb
@@ -11,5 +11,9 @@ end
 get '/compare' do
   @first = params['first']
   @second = params['second']
+  @location1 = Geocoder.search(@first)[0].data['geometry']['location']
+  @location2 = Geocoder.search(@second)[0].data['geometry']['location']
+  @wireline1 = BroadbandMap.wireline(:data_version => 'fall2010', :latitude => @location1['lat'].to_s, :longitude => @location1['lng'].to_s)
+  @wireline2 = BroadbandMap.wireline(:data_version => 'fall2010', :latitude => @location2['lat'].to_s, :longitude => @location2['lng'].to_s)
   erb :compare
 end
