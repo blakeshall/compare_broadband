ec5ab910e9ad415350e402bdf4c5a486305245e4
blakeshall <blake@blakeshall.com>
2011-08-04 15:11:43 -0700

    Added wireless data

--- compare_broadband.rb
+++ compare_broadband.rb
@@ -15,5 +15,7 @@ get '/compare' do
   @location2 = Geocoder.search(@second)[0].data['geometry']['location']
   @wireline1 = BroadbandMap.wireline(:data_version => 'fall2010', :latitude => @location1['lat'].to_s, :longitude => @location1['lng'].to_s)
   @wireline2 = BroadbandMap.wireline(:data_version => 'fall2010', :latitude => @location2['lat'].to_s, :longitude => @location2['lng'].to_s)
+  @wireless1 = BroadbandMap.wireless(:data_version => 'fall2010', :latitude => @location1['lat'].to_s, :longitude => @location1['lng'].to_s)
+  @wireless2 = BroadbandMap.wireless(:data_version => 'fall2010', :latitude => @location2['lat'].to_s, :longitude => @location2['lng'].to_s)
   erb :compare
 end
