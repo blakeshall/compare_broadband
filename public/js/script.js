d41fe1cde7f07327bc4eda220cbb8bb57c2dbd36
blakeshall <blake@blakeshall.com>
2011-08-08 16:25:00 -0700

    Added sortable tables

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,14 @@
-/* Author: 
+/* Author:
 
 */
-
+$(document).ready(function()
+  {
+    $("#wireline_table1").tablesorter();
+    $("#wireline_table2").tablesorter();
+    $("#wireless_table1").tablesorter();
+    $("#wireless_table2").tablesorter();
+  }
+);
 
 
 
