4794c6cab641104a6d83e6aeb2791eb256046e81
blakeshall <blake@blakeshall.com>
2011-08-08 16:45:15 -0700

    Fixed table sorting

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,18 @@
 */
 $(document).ready(function()
   {
-    $("#wireline_table1").tablesorter();
-    $("#wireline_table2").tablesorter();
-    $("#wireless_table1").tablesorter();
-    $("#wireless_table2").tablesorter();
+    $("#wireline_table1").tablesorter({
+      sortList:[[1,1]]
+    });
+    $("#wireline_table2").tablesorter({
+      sortList:[[1,1]]
+    });
+    $("#wireless_table1").tablesorter({
+      sortList:[[1,1]]
+    });
+    $("#wireless_table2").tablesorter({
+      sortList:[[1,1]]
+    });
   }
 );
 
